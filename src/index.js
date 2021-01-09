bdb26bb8b3d9b766f933d54439e938b43aeb283d
douyacun <douyacun@gmail.com>
2021-01-09 20:01:10 +0800

    npm-publish-react-component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 
-function Demo ({name}) {
+function Demo({ name }) {
+    const [duration, setDuration] = useState(0)
+    useEffect(() => {
+        setInterval(() => {
+            setDuration((duration) => {
+                return duration + 1
+            })
+        }, 1000)
+    }, [])
     return (
         <div>
-            <h1>Hello, <span className="name">{name}</span></h1>
+            <h1>Hello, <span className="name">{name}</span> {duration}</h1>
         </div>
     )
 }
--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,26 @@ module.exports = {
     output: {
         path: path.join(__dirname, 'lib'),
         filename: 'index.js',
-        libraryTarget: 'commonjs2',
+        libraryTarget: 'umd',
     },
     resolve: {
         extensions: ['.js', '.jsx']
     },
     mode: "production",
+    externals: { // 重要！Minified React error #321 https://github.com/facebook/react/issues/16029
+        react: {
+            commonjs: 'react',
+            commonjs2: 'react',
+            amd: 'react',
+            root: 'React',
+        },
+        'react-dom': {
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+            root: 'ReactDOM',
+        },
+    },
     module: {
         rules: [
             {
